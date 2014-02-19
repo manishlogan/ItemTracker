edd24d85de51311da51d5e2982d8e811e6a2029f
manishlogan <manishjain.forever@gmail.com>
2014-02-19 19:51:34 +0530

    Added Constants file to keep all constants at one place

---
+++ src/com/evolvingvision/trackyouritems/Constants.java
@@ -0,0 +1,11 @@
+package com.evolvingvision.trackyouritems;
+
+public class Constants {
+	public final static String PERSON_ID = "Person ID";
+	
+	public final static String PERSON_NAME = "Person Name";
+	
+	public final static String CATEGORY_ID = "Category ID";
+	
+	public final static String CATEGORY_NAME = "Category Name";
+}
