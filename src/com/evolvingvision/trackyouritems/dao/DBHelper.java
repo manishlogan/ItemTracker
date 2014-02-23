527dcc16d184be75945cd5629f4a0f5543d1f58f
Manish Logan <manishjain.forever@gmail.com>
2014-02-23 16:46:47 +0530

    Updated DB Schema

--- src/com/evolvingvision/trackyouritems/dao/DBHelper.java
+++ src/com/evolvingvision/trackyouritems/dao/DBHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.evolvingvision.trackyouritems.Constants;
 import com.evolvingvision.trackyouritems.dao.table.CategoryTable;
 import com.evolvingvision.trackyouritems.dao.table.ItemTable;
 import com.evolvingvision.trackyouritems.dao.table.ItemTransactionTable;
@@ -15,7 +16,7 @@ public class DBHelper extends SQLiteOpenHelper{
 
 	private final static String DATABASE_NAME = "TrackYourItems.db";
 	
-	private final static int DATABASE_VERSION = 1;
+	private final static int DATABASE_VERSION = 2;
 	
 	private final static String CREATE_CATEGORY_TABLE = "create table "+
 	CategoryTable.TABLE_NAME + "("+CategoryTable.ID + " Integer Primary Key,"+
@@ -36,7 +37,7 @@ public class DBHelper extends SQLiteOpenHelper{
 	private final static String CREATE_ITEM_TRANSACTION_TABLE = "create table "+
 			ItemTransactionTable.TABLE_NAME + "("+ItemTransactionTable.ID + " Integer Primary Key,"+
 			ItemTransactionTable.ITEM_ID + " integer,"+ItemTransactionTable.CATEGORY_ID+" integer,"+
-			ItemTransactionTable.PERSON_ID+" integer,"+
+			ItemTransactionTable.PERSON_ID+" integer,"+ItemTransactionTable.STATUS_ID+" integer,"+
 			ItemTransactionTable.DUE_DATE+" text)";
 	
 	public DBHelper(Context context) {
@@ -50,12 +51,32 @@ public class DBHelper extends SQLiteOpenHelper{
 		db.execSQL(CREATE_PERSON_TABLE);
 		db.execSQL(CREATE_STATUS_TABLE);
 		db.execSQL(CREATE_ITEM_TRANSACTION_TABLE);
-		initializeCategories(db);
+//		initializeCategories(db);
+		initializeStatus(db);
+	}
+
+	private void initializeStatus(SQLiteDatabase db) {
+		ContentValues lentStatus = new ContentValues();
+		lentStatus.put(StatusTable.ID, Constants.LENT_STATUS);
+		lentStatus.put(StatusTable.STATUS_NAME, "Lent");
+		
+		ContentValues borrowedStatus = new ContentValues();
+		borrowedStatus.put(StatusTable.ID, Constants.BORROWED_STATUS);
+		borrowedStatus.put(StatusTable.STATUS_NAME, "Borrowed");
+		
+		ContentValues returnedStatus = new ContentValues();
+		returnedStatus.put(StatusTable.ID, Constants.RETURNED_STATUS);
+		returnedStatus.put(StatusTable.STATUS_NAME, "Returned");
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+		db.execSQL("drop table "+ItemTransactionTable.TABLE_NAME);
+		db.execSQL("drop table "+CategoryTable.TABLE_NAME);
+		db.execSQL("drop table "+StatusTable.TABLE_NAME);
+		db.execSQL("drop table "+PersonTable.TABLE_NAME);
+		db.execSQL("drop table "+ItemTable.TABLE_NAME);
+		onCreate(db);
 	}
 	
 	private void initializeCategories(SQLiteDatabase db) {
