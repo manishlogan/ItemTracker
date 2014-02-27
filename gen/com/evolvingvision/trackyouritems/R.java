0bc7443c1796ff55d6581516b9ea46f240169077
Manish Logan <manishjain.forever@gmail.com>
2014-02-27 23:35:22 +0530

    Closes #3 Completed flow to add new Lent Item

--- gen/com/evolvingvision/trackyouritems/R.java
+++ gen/com/evolvingvision/trackyouritems/R.java
@@ -71,31 +71,32 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int add=0x7f05001a;
+        public static final int add=0x7f05001b;
         public static final int addBorrowedBtn=0x7f050004;
         public static final int addLentItemBtn=0x7f050005;
-        public static final int add_new_category=0x7f050018;
-        public static final int add_new_person=0x7f050017;
+        public static final int add_new_category=0x7f050019;
+        public static final int add_new_person=0x7f050018;
         public static final int app_name=0x7f050000;
         public static final int borrowedItemsBtn=0x7f050002;
         public static final int borrower=0x7f05000f;
-        public static final int category=0x7f050012;
-        public static final int due_date=0x7f050013;
+        public static final int category=0x7f050013;
+        public static final int due_date=0x7f050014;
         public static final int enter_item_name=0x7f05000e;
         public static final int hello_world=0x7f050008;
         public static final int item_name=0x7f05000d;
+        public static final int lender=0x7f050010;
         public static final int lentItemsBtn=0x7f050003;
-        public static final int name=0x7f050011;
-        public static final int name_borrower=0x7f050010;
-        public static final int save=0x7f050014;
+        public static final int name=0x7f050012;
+        public static final int name_borrower=0x7f050011;
+        public static final int save=0x7f050015;
         public static final int title_activity_add_borrowed_item=0x7f05000b;
-        public static final int title_activity_add_category=0x7f05001b;
+        public static final int title_activity_add_category=0x7f05001c;
         public static final int title_activity_add_lent_item=0x7f05000a;
-        public static final int title_activity_add_person=0x7f050019;
+        public static final int title_activity_add_person=0x7f05001a;
         public static final int title_activity_borrowed_items=0x7f050007;
         public static final int title_activity_lent_items=0x7f050009;
-        public static final int title_activity_select_category=0x7f050016;
-        public static final int title_activity_select_person=0x7f050015;
+        public static final int title_activity_select_category=0x7f050017;
+        public static final int title_activity_select_person=0x7f050016;
         public static final int title_activity_view=0x7f05000c;
         public static final int viewActivityBtn=0x7f050006;
     }
--- src/com/evolvingvision/trackyouritems/AddLentItemActivity.java
+++ src/com/evolvingvision/trackyouritems/AddLentItemActivity.java
@@ -1,11 +1,23 @@
 package com.evolvingvision.trackyouritems;
 
+import com.evolvingvision.trackyouritems.dao.TrackYourItemsDao;
+import com.evolvingvision.trackyouritems.entity.Category;
+import com.evolvingvision.trackyouritems.entity.Person;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddLentItemActivity extends Activity {
 
+	private Person person;
+	
+	private Category category;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,4 +31,45 @@ public class AddLentItemActivity extends Activity {
 		return true;
 	}
 
+	public void selectPerson(View view){
+		Intent nextActivity = new Intent(this, SelectPersonActivity.class);
+		startActivityForResult(nextActivity, 201);
+	}
+	
+	public void selectCategory(View view){
+		Intent nextActivity = new Intent(this, SelectCategoryActivity.class);
+		startActivityForResult(nextActivity, 202);	
+	}
+	
+	public void save(View view){
+		EditText itemNameTxt = (EditText)findViewById(R.id.itemName);
+		String itemName = itemNameTxt.getText().toString();
+		long itemID = TrackYourItemsDao.addItem(this, itemName);
+		long transactionID = TrackYourItemsDao.addTransaction(this,itemID,category.getCategoryID(),Constants.LENT_STATUS,person.getPersonID());
+		if(transactionID != -1){
+			Toast.makeText(this, "Lent Item Added", Toast.LENGTH_SHORT).show();
+		}else{
+			Toast.makeText(this, "Failed to add Lent Item", Toast.LENGTH_SHORT).show();
+		}
+		
+		Intent nextActivity = new Intent(this, MainActivity.class);
+		startActivity(nextActivity);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == 201 && resultCode == 201){
+			EditText personName = (EditText)findViewById(R.id.personName);
+			String name = data.getStringExtra(Constants.PERSON_NAME);
+			long id = data.getLongExtra(Constants.PERSON_ID,0);
+			person = new Person(id, name);
+			personName.setText(name);
+		}else if(requestCode == 202 && resultCode == 202){
+			EditText categoryNameText = (EditText)findViewById(R.id.categoryName);
+			String categoryName = data.getStringExtra(Constants.CATEGORY_NAME);
+			long id = data.getLongExtra(Constants.CATEGORY_ID, 0); 
+			category = new Category(id, categoryName);
+			categoryNameText.setText(categoryName);
+		}
+	}
 }
